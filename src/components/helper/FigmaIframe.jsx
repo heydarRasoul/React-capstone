766d861cc5d59ffb02eddf6b69c058b19ebc98d1
Heydar Rasoulpouranaei <heydarrasoulpour@ddevpipeline.com>
2025-08-14 16:24:11 -0600

    Adding figma iframe to about page

---
+++ src/components/helper/FigmaIframe.jsx
@@ -0,0 +1,7 @@
+export default function VideoIframe() {
+  return (
+    <div className="iframe-wrapper">
+      <iframe src="https://embed.figma.com/design/UK17jpDLpD7p4VZ6p9q7Qn/One-place-shop?node-id=0-1&embed-host=share"></iframe>
+    </div>
+  );
+}
--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -101,6 +101,9 @@ export default function About() {
           </p>
         </div>
       </div>
+      <div className="iframe-wrapper">
+        <iframe src="https://embed.figma.com/design/UK17jpDLpD7p4VZ6p9q7Qn/One-place-shop?node-id=0-1&embed-host=share"></iframe>
+      </div>
     </div>
   );
 }
